a213b8c8024f3e532bcfddb80e806caa2bd3f881
dboots <don@donboots.com>
2016-01-25 16:49:20 -0500

    removed schedule console.log

--- server/app/schedule/schedule.js
+++ server/app/schedule/schedule.js
@@ -2,5 +2,4 @@ var cron = require('node-schedule');
 var rule = new cron.RecurrenceRule();
 rule.second = 30;
 cron.scheduleJob(rule, function(){
-    //console.log(new Date(), 'The 30th second of the minute.');
 });
