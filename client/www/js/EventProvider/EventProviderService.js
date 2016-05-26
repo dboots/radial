f18f562aad0cbb7ca3380f9cfb487f74e496e213
dboots <don@donboots.com>
2016-05-26 12:42:45 -0400

    code refactor

--- client/www/js/EventProvider/EventProviderService.js
+++ client/www/js/EventProvider/EventProviderService.js
@@ -1,72 +1,88 @@
 (function () {
 	'use strict';
 
-	angular.module('app.controllers')
-		.factory('EventProviderService', ['$global', '$http', '$q', function($global, $http, $q) {
-			var _proxy = ($global.config('local')) ? 'http://Dons-MBP:1337/' : 'http://';
-			var _providers = [];
+	angular
+		.module('app.controllers')
+		.factory('EventProviderService', EventProviderService);
 
-			var EventProviderService = {
-				LoadProviders: function() {
-					$http.get($global.config('api') + '/event/providers');
-				},
+	EventProviderService.$inject = ['$global', '$http', '$q'];
 
-				Providers: function() {
-					var defer = $q.defer();
+	function EventProviderService($global, $http, $q) {
+		var _proxy = ($global.config('local')) ? 'http://Dons-MBP:1337/' : 'http://';
+		var _providers = [];
 
-					if (_providers.length)
-						defer.resolve(_providers);
+		var service = {
+			loadProviders: loadProviders,
+			providers: providers,
+			get: get
+		};
 
-					EventProviderService.LoadProviders().then(function(res) {
-						defer.resolve(res.data);
-					});
+		return service;
 
-					return defer.promise;
-				},
+		//////////////////////////////////
 
-				/*
-				
-				my_provider = {
-					id: 'eventful'
-					url: 'api.eventful.com/json/events/search',
-					key: 'fvB6CnMq97xZBXLL'
+		function loadProviders() {
+			return $http.get($global.config('api') + '/event/providers', {
+				params: {
+					token: localStorage.getItem('token')
 				}
-
-				my_location = {
-					latitude: 0,
-					longitude: 0
-				}
-
-				Options specific to provider
-				my_opts = {
-					within: 100,
-					category: 1,2,3
-				}
-
-				*/
-				
-				Get: function(my_provider, my_location, my_opts) {
-					var url = _getUrl(my_provider, my_location, my_opts);
-					console.log('[EventProviderService:Search] using url:', url);
-					return $http.get(url);
-				}
-			};
-
-			return EventProviderService;
-		}]);
-
-	function _getUrl(my_provider, my_location, my_opts) {
-		//-- Create base API url
-		var url = _proxy + my_provider.url + '?key=' + my_provider.key;
-
-		//-- Append location details
-		url += '&location=' + my_location.latitude + ',' + my_location.longitude;
-
-		//-- Construct passed opts
-		for (var prop in my_opts) {
-			url += '&' + prop + '=' + my_opts[prop];
+			});
 		}
 
-		return url;
+		function providers() {
+			var defer = $q.defer();
+
+			if (_providers.length)
+				defer.resolve(_providers);
+
+			loadProviders().then(function(res) {
+				defer.resolve(res.data.data);
+			});
+
+			return defer.promise;
+		}
+		/*
+		
+		my_provider = {
+			id: 'eventful'
+			url: 'api.eventful.com/json/events/search',
+			key: 'fvB6CnMq97xZBXLL'
+		}
+
+		my_location = {
+			latitude: 0,
+			longitude: 0
+		}
+
+		Options specific to provider. This would most likely be used for params specific to each
+		API
+		my_opts = {
+			app_key: 'xxxxxxx'
+			within: 100,
+			category: 1,2,3
+		}
+
+		*/
+		
+		function get(my_provider, my_location, my_opts) {
+			var url = _getUrl(my_provider, my_location, my_opts);
+			console.log('[EventProviderService:Search] using url:', url);
+			return $http.get(url);
+		}
+
+		function _getUrl(my_provider, my_location, my_opts) {
+			//-- Create base API url
+			var url = _proxy + my_provider.url + '?1=1';
+
+			//-- Append location details
+			url += '&location=' + my_location.latitude + ',' + my_location.longitude;
+
+			//-- Construct passed opts
+			for (var prop in my_opts) {
+				url += '&' + prop + '=' + my_opts[prop];
+			}
+
+			return url;
+		}
 	}
 })();
